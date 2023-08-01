af6ead90cc5c9f32b9d1168c5b2e0de0fd2008df
BM21BCS4526 <136880460+BM21BCS4526@users.noreply.github.com>
2023-08-01 13:58:01 +0530

    Update getBalance.js

--- getBalance.js
+++ getBalance.js
@@ -1,9 +1,9 @@
 const hre = require("hardhat");
 const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
 
-const tokenAddress = "0x08e450d7BA9842d1C2095e85A04FBaC88E726171"; // Place your ERC20 contract address here
+const tokenAddress = "0x08e450d7BA9842d1C2095e85A04FBaC88E726171";
 const tokenABI = tokenContractJSON.abi;
-const walletAddress = "0xCBff759a27f7A81925ef44081B9B5417fE529DD7"; // Place your public address for your wallet here
+const walletAddress = "0xCBff759a27f7A81925ef44081B9B5417fE529DD7"; 
 
 async function main() {
   const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
