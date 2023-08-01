71ad4b219331124f81c9390dafd7ba2dc05d9a8c
BM21BCS4526 <136880460+BM21BCS4526@users.noreply.github.com>
2023-08-01 11:36:02 +0530

    Create getBalance.js

---
+++ getBalance.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
+
+const tokenAddress = "0x08e450d7BA9842d1C2095e85A04FBaC88E726171"; // Place your ERC20 contract address here
+const tokenABI = tokenContractJSON.abi;
+const walletAddress = "0xCBff759a27f7A81925ef44081B9B5417fE529DD7"; // Place your public address for your wallet here
+
+async function main() {
+  const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+
+  const balance = await token.balanceOf(walletAddress);
+  console.log(`Balance of tokens for address ${walletAddress}: ${balance}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
