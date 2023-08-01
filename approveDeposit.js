3ae4d9095983b467f6bb4b23bafde682e2895702
BM21BCS4526 <136880460+BM21BCS4526@users.noreply.github.com>
2023-08-01 13:58:26 +0530

    Update approveDeposit.js

--- approveDeposit.js
+++ approveDeposit.js
@@ -2,10 +2,10 @@ const hre = require("hardhat");
 const fxRootContractABI = require("../fxRootContractABI.json");
 const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
 
-const tokenAddress = "0x55Cb406C711B338A96D540850774BA4c01EB4849"; // Place your ERC721 contract address here
+const tokenAddress = "0x55Cb406C711B338A96D540850774BA4c01EB4849";
 const tokenABI = tokenContractJSON.abi;
 const fxERC20RootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
-const walletAddress = "0xCBff759a27f7A81925ef44081B9B5417fE529DD7"; // Place your public address for your wallet here
+const walletAddress = "0xCBff759a27f7A81925ef44081B9B5417fE529DD7"; 
 
 const nftTokenIds = [1, 2, 3, 4, 5]; // Array of token IDs to transfer
 
